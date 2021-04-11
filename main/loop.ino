6cf657329013a3bb4274cf7a3b62b3fcf9b8d5d3
PeterRichardson01 <43798594+PeterRichardson01@users.noreply.github.com>
2021-04-11 15:01:51 -0400

    Update loop.ino

--- main/loop.ino
+++ main/loop.ino
@@ -20,14 +20,8 @@ void loop() {
   sensors_event_t event;
   bno.getEvent(&event);
 
-  if(millis() < 9500)
-  {
-    desalt = 0.1;
-  }
-  else
-  {
-    desalt = -1.0;
-  }
+  desalt = 0.2;
+
 
   double thrust = thrustCTL.calculate(desalt, alt[0]);
   double pitch = pitchCTL.calculate(0, event.orientation.y);
@@ -41,42 +35,38 @@ void loop() {
   {
     yaw = yawCTL.calculate(0, event.orientation.x - 360.0);
   }
-  /*
+  
   //front motors = thrust - pitch
   //back motors = thrust + pitch
   //left motors = thrust + roll
   //right motors = thrust - roll
   
-  
-  Serial.println(millis());
-  */
+  //Motors UL-UR-BL-BR --> 1-2-3-4
 
-  
-  if(millis() < 0)//10500)
+  if(millis() < 20000)
   {
-    //Motors UL-UR-BL-BR --> 1-2-3-4
-    motor1.writeMicroseconds(1320 - pitch + roll + yaw);
-    motor2.writeMicroseconds(1320 - pitch - roll - yaw);
-    motor3.writeMicroseconds(1320 + pitch + roll - yaw);
-    motor4.writeMicroseconds(1320 + pitch - roll + yaw);
-
-    /*
-    Serial.print(event.orientation.x);
-    Serial.print("\t");
-    Serial.println(yaw);
-    
-    Serial.println(SAMPLERATE/1000.0);
-    Serial.println(yawCTL.getDT());
-    Serial.println(yawCTL.getinteg());
-    */
-  }/*
+  motor1.writeMicroseconds(1440 + thrust - pitch + roll + yaw);
+  motor2.writeMicroseconds(1440 + thrust - pitch - roll - yaw);
+  motor3.writeMicroseconds(1440 + thrust + pitch + roll - yaw);
+  motor4.writeMicroseconds(1440 + thrust + pitch - roll + yaw);
+  }
   else
   {
-    motor1.writeMicroseconds(1000);
-    motor2.writeMicroseconds(1000);
-    motor3.writeMicroseconds(1000);
-    motor4.writeMicroseconds(1000);
-  }*/
+  motor1.writeMicroseconds(1000);
+  motor2.writeMicroseconds(1000);
+  motor3.writeMicroseconds(1000);
+  motor4.writeMicroseconds(1000);
+  }
+
+  /*
+  Serial.print(event.orientation.x);
+  Serial.print("\t");
+  Serial.println(yaw);
+  
+  Serial.println(SAMPLERATE/1000.0);
+  Serial.println(yawCTL.getDT());
+  Serial.println(yawCTL.getinteg());
+  */
 
   /*
   Serial.print(millis());
@@ -85,8 +75,20 @@ void loop() {
   Serial.print("\t");
   Serial.println(bmp.readAltitude(bmpOffset));
   */
+  /*
+  Serial.print("Y: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.println(event.orientation.z, 4);
+  
   Serial.print(pitch);
   Serial.print("\t");
   Serial.println(roll);
+  */
+
+  //Serial.println(event.orientation.x);
+  //Serial.println(yaw);
+  
   while((micros() - tStart) < (SAMPLERATE * 1000)){}
+  //Serial.println(micros()-tStart);
 }
