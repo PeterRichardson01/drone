968f20e14dbda5bb1efa937620119ee02d594c1c
PeterRichardson01 <43798594+PeterRichardson01@users.noreply.github.com>
2021-04-22 13:17:08 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -45,7 +45,7 @@ PID yawCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 5.0, 2.0, 0.05);
 */
 PID pitchCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 0.5, 0.0, 0.7);
 PID rollCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 0.5, 0.0, 0.7);
-PID yawCTL = PID(SAMPLERATE/1000.0, 100.0, -100.0, 4.0, 1.0, 0.0);
+PID yawCTL = PID(SAMPLERATE/1000.0, 100.0, -100.0, 4.0, 0.1, 0.0);
 
 Servo motor1;
 Servo motor2;
