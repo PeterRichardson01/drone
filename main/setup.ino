a75edb082d8f3bd30473972d49ac0bd6561a3ccb
PeterRichardson01 <43798594+PeterRichardson01@users.noreply.github.com>
2021-05-02 16:43:35 -0400

    Update setup.ino

--- main/setup.ino
+++ main/setup.ino
@@ -1,10 +1,74 @@
+//Pin change interrupts for RC Receiver PWM signals
+
+void rising1()
+{
+  prev_time1 = micros();
+  PCintPort::detachInterrupt(CH1PIN);
+  PCintPort::attachInterrupt(CH1PIN, &falling1, FALLING);
+}
+
+void falling1()
+{
+  ch1 = micros()-prev_time1;
+  PCintPort::detachInterrupt(CH1PIN);
+  PCintPort::attachInterrupt(CH1PIN, &rising1, RISING);
+}
+
+void rising2()
+{
+  prev_time2 = micros();
+  PCintPort::detachInterrupt(CH2PIN);
+  PCintPort::attachInterrupt(CH2PIN, &falling2, FALLING);
+
+}
+
+void falling2()
+{
+  ch2 = micros()-prev_time2;
+  PCintPort::detachInterrupt(CH2PIN);
+  PCintPort::attachInterrupt(CH2PIN, &rising2, RISING);
+}
+
+void rising3()
+{
+  prev_time3 = micros();
+  PCintPort::detachInterrupt(CH3PIN);
+  PCintPort::attachInterrupt(CH3PIN, &falling3, FALLING);
+}
+
+void falling3()
+{
+  ch3 = micros()-prev_time3;
+  PCintPort::detachInterrupt(CH3PIN);
+  PCintPort::attachInterrupt(CH3PIN, &rising3, RISING);
+}
+
+void rising4()
+{
+  prev_time4 = micros();
+  PCintPort::detachInterrupt(CH4PIN);
+  PCintPort::attachInterrupt(CH4PIN, &falling4, FALLING);
+}
+
+void falling4()
+{
+  PCintPort::detachInterrupt(CH4PIN);
+  PCintPort::attachInterrupt(CH4PIN, &rising4, RISING);
+  ch4 = micros()-prev_time4;
+}
+
 void setup() {
   //Serial.begin(115200);
 
-  pinMode(CH1PIN, INPUT);
-  pinMode(CH2PIN, INPUT);
-  pinMode(CH3PIN, INPUT);
-  pinMode(CH4PIN, INPUT);
+  pinMode(CH1PIN, INPUT_PULLUP);
+  pinMode(CH2PIN, INPUT_PULLUP);
+  pinMode(CH3PIN, INPUT_PULLUP);
+  pinMode(CH4PIN, INPUT_PULLUP);
+
+  PCintPort::attachInterrupt(CH1PIN, &rising1, RISING);
+  PCintPort::attachInterrupt(CH2PIN, &rising2, RISING);
+  PCintPort::attachInterrupt(CH3PIN, &rising3, RISING);
+  PCintPort::attachInterrupt(CH4PIN, &rising4, RISING);
   
   motor1.attach(MOTOR1PIN);
   motor2.attach(MOTOR2PIN);
@@ -38,7 +102,7 @@ void setup() {
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
   
-  delay(5000);
+  delay(6000);
   
   motor1.writeMicroseconds(1435);
   motor2.writeMicroseconds(1445);
@@ -57,4 +121,6 @@ void setup() {
   //set Offsets to set altitude to 0 at ground and yaw at 0
   bmpOffset = bmp.pressure/100.0;
   desalt = 0.0;
+
+  tStart = micros();
 }
