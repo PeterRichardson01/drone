8e9e5491e90bbc003fc1782070227d4bc21a8ac7
PeterRichardson01 <43798594+PeterRichardson01@users.noreply.github.com>
2021-05-04 17:03:27 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -14,7 +14,7 @@
 #define BMP_MOSI 11
 #define BMP_CS 10
  
-#define SAMPLERATE (25.0) //40 Hz
+#define SAMPLERATE (10.0) //100 Hz
 
 //Motors UL-UR-BL-BR --> 1-2-3-4
 #define MOTOR1PIN 11
@@ -45,16 +45,16 @@ double despitch;
 double desroll;
 double desyaw;
 
-PID thrustCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 1.0, 0.005, 0.02);
+PID thrustCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 0.0, 0.000, 0.00);
 /*
 PID pitchCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 5.0, 2.0, 0.05);
 PID rollCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 5.0, 2.0, 0.05);
 PID yawCTL = PID(SAMPLERATE/1000.0, 10.0, -10.0, 5.0, 2.0, 0.05);
 */
 
-PID pitchCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 1.0, 0.005, 0.02);
-PID rollCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 1.0, 0.02, 0.02);
-PID yawCTL = PID(SAMPLERATE/1000.0, 100.0, -100.0, 4.0, 0.1, 0.0);
+PID pitchCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 0.7, 0.01, 0.01);
+PID rollCTL = PID(SAMPLERATE/1000.0, 15.0, -15.0, 0.7, 0.01, 0.01);
+PID yawCTL = PID(SAMPLERATE/1000.0, 100.0, -100.0, 4.0, 0.08, 0.0);
 
 Servo motor1;
 Servo motor2;
