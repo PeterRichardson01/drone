6a663d5f79a6b964c3a07c45de0f8ddadfb238a4
PeterRichardson01 <43798594+PeterRichardson01@users.noreply.github.com>
2021-05-04 17:03:14 -0400

    Update loop.ino

--- main/loop.ino
+++ main/loop.ino
@@ -1,24 +1,25 @@
 
 void loop() {
-
+  
+  /*
+  unsigned long test = micros();
   for(int i = 4;i > 0; i--)
   {
     alt[i] = alt[i-1];
   }
 
-  if (! bmp.performReading())
-  {
-    desalt = 0.0;
-    alt[0] = 0.1;
-  }
-  else
-  {
-    alt[0] = (alt[1] + alt[2] + alt[3] + alt[4] + bmp.readAltitude(bmpOffset))/5.0;
-  }
-
+  alt[0] = (alt[1] + alt[2] + alt[3] + alt[4] + bmp.readAltitude(bmpOffset))/5.0;
+    
+  Serial.print(micros() - test);
+  Serial.println(": Pressure reading and boxcar filter");
+  */
+  
+  //unsigned long test = micros();
   sensors_event_t event;
   bno.getEvent(&event);
-
+  //Serial.print(micros() - test);
+  //Serial.println(": Orientation reading");
+  
   /*
   unsigned long test = millis();
   ch1 = pulseIn(CH1PIN, HIGH); //altitude
@@ -29,12 +30,15 @@ void loop() {
   //channel values now between 1000 min and 2000 max
   */
 
-  desalt = desalt + (double(ch1) - 1500.0)/500.0*SAMPLERATE/1000.0; //changes within 1 m/s
-  despitch = (double(ch3) - 1500.0)/50.0; //angle within +/- 10 degrees
-  desroll = (double(ch4) - 1500.0)/50.0; //angle within +/- 10 degrees
+  //test = micros();
+  //desalt = desalt + (double(ch1) - 1500.0)/500.0*SAMPLERATE/1000.0; //changes within 1 m/s
+  double thrust = (double(ch1) - 1100.0)/25.0; //thrust within 0 and 36
+  despitch = (double(ch3) - 1500.0)/10.0; //angle within +/- 50 degrees
+  desroll = (double(ch4) - 1500.0)/10.0; //angle within +/- 50 degrees
   desyaw = (double(ch2) - 1500.0)*150.0/500.0; // angle within +/- 150 degrees
+  //Serial.print(micros() - test);
+  //Serial.println(": clamping controller input");
   
-
   /*
   desalt = 0;
   despitch = 0;
@@ -42,8 +46,9 @@ void loop() {
   desyaw = 0;
   */
 
-  double thrust = thrustCTL.calculate(desalt, alt[0]);
-  double pitch = pitchCTL.calculate(despitch, event.orientation.y);
+  //test = micros();
+  //double thrust = thrustCTL.calculate(desalt, alt[0]);
+  double pitch = pitchCTL.calculate(despitch, event.orientation.y); 
   double roll  = rollCTL.calculate (desroll, event.orientation.z);
   double yaw;
   if(event.orientation.x < 180.0)
@@ -55,13 +60,17 @@ void loop() {
     yaw = yawCTL.calculate(desyaw, event.orientation.x - 360.0);
   }
   
+  //Serial.print(micros() - test);
+  //Serial.println(": PID calculations");
+  
   //front motors = thrust - pitch
   //back motors = thrust + pitch
   //left motors = thrust + roll
   //right motors = thrust - roll
   
   //Motors UL-UR-BL-BR --> 1-2-3-4
-  
+
+  //test = micros();
   if(ch1 < 1050)
   {
     motor1.writeMicroseconds(1000);
@@ -71,11 +80,13 @@ void loop() {
   }
   else
   {
-    motor1.writeMicroseconds(1440 + (int)(thrust - pitch + roll + yaw));
-    motor2.writeMicroseconds(1450 + (int)(thrust - pitch - roll - yaw));
-    motor3.writeMicroseconds(1450 + (int)(thrust + pitch + roll - yaw));
-    motor4.writeMicroseconds(1440 + (int)(thrust + pitch - roll + yaw));
+    motor1.writeMicroseconds(1460 + (int)(thrust - pitch + roll + yaw));
+    motor2.writeMicroseconds(1470 + (int)(thrust - pitch - roll - yaw));
+    motor3.writeMicroseconds(1470 + (int)(thrust + pitch + roll - yaw));
+    motor4.writeMicroseconds(1460 + (int)(thrust + pitch - roll + yaw));
   }
+  //Serial.print(micros() - test);
+  //Serial.println(": Outputting to ESCs");
 
   /*
   Serial.print(event.orientation.x);
@@ -120,6 +131,9 @@ void loop() {
   Serial.print("\t");
   Serial.println(ch4);
   */
+  
+  //Serial.print(micros() - tStart);
+  //Serial.println(": Total loop time");
   
   while((micros() - tStart) < (SAMPLERATE * 1000)){}
   //Serial.println(micros()-tStart);
